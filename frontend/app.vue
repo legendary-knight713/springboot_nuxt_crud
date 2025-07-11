1daf331ed1b6389841f7ec6e6a010a3125a8a126
legendary <legendary.knight713@gmail.com>
2025-07-11 02:04:34 +0200

    Basic State Management

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout >
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+const count = ref(1)
+</script>
